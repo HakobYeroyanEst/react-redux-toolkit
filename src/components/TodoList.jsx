64953fe6c9adbc1ad19864acc2bc12218bb03db0
JacobGlitcher <hakob.yer@gmail.com>
2024-03-13 16:08:35 +0400

    fixed warning issue

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,17 +1,19 @@
-import { useSelector } from 'react-redux'
-import TodoItem from './TodoItem'
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import TodoItem from "./TodoItem"
 
 const TodoList = () => {
-  const todos = useSelector((state) => {
-    const { todos, searchTerm } = state.todo
+  const { todos, searchTerm } = useSelector((state) => state.todo)
+  const [filteredTodos, setFilteredTodos] = useState([])
 
-    return todos?.filter((todo) => todo.text?.includes(searchTerm))
-  })
+  useEffect(() => {
+    setFilteredTodos(todos?.filter((todo) => todo.text?.includes(searchTerm)))
+  }, [searchTerm, todos])
 
   return (
     <ul>
       <li className="my-2 text-sm italic">All Your Notes Here...</li>
-      {todos.map((todoItem, index) => (
+      {filteredTodos.map((todoItem, index) => (
         <TodoItem key={index} todoItem={todoItem} index={index} />
       ))}
     </ul>
